c6a5a339a57a5661fec289f321da9d70e910859f
sri-dharshini-revathi <67547021+sri-dharshini-revathi@users.noreply.github.com>
2021-07-30 19:19:46 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import RandomImage from './components/RandomImage';
+
+function App() {
+  return (
+    <div className='App'>
+        <RandomImage />
+    </div>
+  )
+}
+
+export default App;
